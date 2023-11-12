b582461acbf0428e02733a8dc90bd243ca23b06b
xingyu <xingyu4j@vip.qq.com>
2023-11-12 16:55:13 +0800

    chore: add fast crud

---
+++ src/views/crud/demo/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-full">
+    <fs-crud ref="crudRef" v-bind="crudBinding"></fs-crud>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { useFs } from '@fast-crud/fast-crud';
+import createCrudOptions from './crud';
+const { crudRef, crudBinding, crudExpose } = useFs({ createCrudOptions });
+// 页面打开后获取列表数据
+onMounted(() => {
+  crudExpose.doRefresh();
+});
+</script>
